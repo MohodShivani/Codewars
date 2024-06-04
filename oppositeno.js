367b976569e6dd699c2be3730921993540e1720b
MohodShivani <139542190+MohodShivani@users.noreply.github.com>
2024-06-04 12:51:03 +0530

    Additive inverse of a number.

---
+++ oppositeno.js
@@ -0,0 +1,7 @@
+//Very simple, given a number (integer / decimal / both depending on the language), find its opposite (additive inverse).
+//Examples: 1: -1
+
+//My Solution:
+   function opposite(number) {
+      return(-number)
+   }
