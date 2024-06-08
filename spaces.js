32af7a89643f7142d250c8f0baf992984ff604db
MohodShivani <139542190+MohodShivani@users.noreply.github.com>
2024-06-08 12:13:57 +0530

    Remove string spaces

---
+++ spaces.js
@@ -0,0 +1,8 @@
+//Write a function that removes the spaces from the string, then return the resultant string.
+//Examples:
+//Input -> Output : "8 j 8   mBliB8g  imjB8B8  jl  B" -> "8j8mBliB8gimjB8B8jlB"
+
+//My Solution:
+function noSpace(x){
+  return x.split(' ').join('')
+}
