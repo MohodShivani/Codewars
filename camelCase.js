3b3ef06b1bcf794e443012d719f4b4916db99e9a
MohodShivani <139542190+MohodShivani@users.noreply.github.com>
2024-06-17 09:41:10 +0530

    Break camelCase

---
+++ camelCase.js
@@ -0,0 +1,7 @@
+//Complete the solution so that the function will break up camel casing, using a space between words.
+//Example: "camelCasing"  =>  "camel Casing"
+
+//My Solution:
+function solution(string){
+  return string.replace(/([a-z])([A-Z])/g, '$1 $2')
+}
