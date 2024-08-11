9086c5959a03fd7f3450187da193aa0c0a4fe828
MohodShivani <139542190+MohodShivani@users.noreply.github.com>
2024-08-11 12:40:13 +0530

    Grasshopper: Messi goals function.

---
+++ messigoals.js
@@ -0,0 +1,8 @@
+//Complete the function to return his total number of goals in all three leagues.
+//For example: 5, 10, 2  -->  17
+
+//My Solution:
+function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+  // code goes here
+  return (laLigaGoals + copaDelReyGoals + championsLeagueGoals)
+}
