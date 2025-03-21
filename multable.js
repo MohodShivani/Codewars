46a82580914f54f106c416db0233475e8eec1b32
MohodShivani <139542190+MohodShivani@users.noreply.github.com>
2025-03-21 12:41:36 +0530

    Multiplication table for number

---
+++ multable.js
@@ -0,0 +1,30 @@
+Your goal is to return multiplication table for number that is always an integer from 1 to 10.
+For example, a multiplication table (string) for number == 5 looks like below:
+1 * 5 = 5
+2 * 5 = 10
+3 * 5 = 15
+4 * 5 = 20
+5 * 5 = 25
+6 * 5 = 30
+7 * 5 = 35
+8 * 5 = 40
+9 * 5 = 45
+10 * 5 = 50
+//My Solution:
+const multiTable = (number) => {
+  let table = '';
+  for(let i = 1; i <= 10; i++) {
+    table += `${i} * ${number} = ${i * number}${i < 10 ? '\n' : ''}`; 
+  }
+  return table;
+}
+
+OR
+
+const multiTable = (n) => {
+	const table = []
+	for(let i = 1; i <= 10; i++) {
+		table.push(`${i} * ${n} = ${i*n}`)
+	}
+	return table.join('\n')
+}
