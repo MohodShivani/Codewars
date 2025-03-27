68453b8c04e7f24b00b40682788eaa138f5019b8
MohodShivani <139542190+MohodShivani@users.noreply.github.com>
2025-03-27 10:15:04 +0530

    Invert Values.

---
+++ invertval.js
@@ -0,0 +1,8 @@
+Given a set of numbers, return the additive inverse of each. Each positive becomes negatives, and the negatives become positives.
+[1, 2, 3, 4, 5] --> [-1, -2, -3, -4, -5]
+[1, -2, 3, -4, 5] --> [-1, 2, -3, 4, -5]
+[] --> []
+//My Solution:
+  function invert(array) {
+   return array.map(x=>-x)
+}
