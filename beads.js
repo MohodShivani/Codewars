ebd1ed12c5592283de2c86458cd5ed5c3c30272d
MohodShivani <139542190+MohodShivani@users.noreply.github.com>
2025-05-10 12:03:41 +0530

    Simple beads count

---
+++ beads.js
@@ -0,0 +1,9 @@
+Two red beads are placed between every two blue beads. There are N blue beads. After looking at the arrangement below work out the number of red beads.
+@ @@ @ @@ @ @@ @ @@ @ @@ @
+Implement a function returning the number of red beads.
+If there are less than 2 blue beads return 0.
+//My Solution:
+function countRedBeads(n) {
+  return (n<2) ? 0 : (n*2)-2
+  
+}
