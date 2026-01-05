c921313efd429a340ffa930d775a092798eb2a8b
ShivaniMohod <139542190+MohodShivani@users.noreply.github.com>
2026-01-05 17:43:37 +0530

    Multiplication Table.

---
+++ multiplicationtable.js
@@ -0,0 +1,19 @@
+Your task, is to create N×N multiplication table, of size provided in parameter.
+For example, when given size is 3:
+
+1 2 3
+2 4 6
+3 6 9
+For the given example, the return value should be:
+[[1,2,3],[2,4,6],[3,6,9]]
+//My Solution:
+const multiplicationTable = n => {
+  const r = [];
+  for (let i = 1; i <= n; i++) {
+    const x = [];
+    for (let j = 1; j <= n; j++)
+      x.push(i * j);
+    r.push(x);
+  }
+  return r;
+}
