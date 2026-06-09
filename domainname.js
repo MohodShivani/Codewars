ad12b39d544e3e43e9bde7497a80861e27554de3
ShivaniMohod <139542190+MohodShivani@users.noreply.github.com>
2026-06-09 16:14:27 +0530

    Domain name

---
+++ domainname.js
@@ -0,0 +1,11 @@
+Write a function that when given a URL as a string, parses out just the domain name and returns it as a string. For example:
+* url = "http://github.com/carbonfive/raygun" -> domain name = "github"
+//Solution:
+
+function domainName(url){
+   return url
+    .replace('http://', '')
+    .replace('https://', '')
+    .replace('www.', '')
+    .split('.')[0];
+}
